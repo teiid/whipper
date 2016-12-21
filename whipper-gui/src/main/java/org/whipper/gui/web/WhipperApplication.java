db0248d696dccebaa278e614ff9772f48bacc24e
Juraj Duráni <jdurani@redhat.com>
2016-12-21 08:42:30 +0100

    initial Web UI

---
+++ whipper-gui/src/main/java/org/whipper/gui/web/WhipperApplication.java
@@ -0,0 +1,7 @@
+package org.whipper.gui.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("whipper-ui")
+public class WhipperApplication extends Application{}
